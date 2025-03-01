ad604e5ecd6e4b1fe4974bbf48bf7d8972686ca1
amresh-das <mail2amreshdas@gmail.com>
2025-03-01 04:04:24 -0500

    Step 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -4,4 +4,7 @@ def emotion_detector(text_to_analyze):
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     body = { "raw_document": { "text": text_to_analyze } }
-    return requests.post(url, headers = headers, json = body).text
+    jsonObj = requests.post(url, headers = headers, json = body).json()
+    emotionObj = jsonObj['emotionPredictions'][0]['emotion']
+    emotionObj['dominant_emotion'] = max(emotionObj, key=emotionObj.get)
+    return emotionObj
