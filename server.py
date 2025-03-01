58df3a5180c7c8286c966d174e779c71c9649b50
amresh-das <mail2amreshdas@gmail.com>
2025-03-01 18:41:46 +0530

    Final Step

--- server.py
+++ server.py
@@ -1,12 +1,19 @@
 """
 Configures server for running emotion detection
 """
-from flask import Flask, request
+from flask import Flask, request, render_template
 import EmotionDetection
 
 app = Flask("emotionDetector")
 
-@app.route("/emotionDetector", methods=["POST"])
+@app.route("/")
+def render_index_page():
+    """
+    Renders index page
+    """
+    return render_template("index.html")
+
+@app.route("/emotionDetector", methods=["GET", "POST"])
 def emotion_detector():
     """
     Invokes sentiment analysis on provided string
