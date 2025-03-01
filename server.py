9f94b972aae0649877754a8e1e81d19b92a702ac
amresh-das <mail2amreshdas@gmail.com>
2025-03-01 04:56:12 -0500

    Step 7

--- server.py
+++ server.py
@@ -6,4 +6,7 @@ app = Flask("emotionDetector")
 @app.route("/emotionDetector", methods=["POST"])
 def emotion_detector():
     text = request.args.get('text_to_analyze')
-    return EmotionDetection.emotion_detection.emotion_detector(text)
+    response = EmotionDetection.emotion_detection.emotion_detector(text)
+    if response['dominant_emotion'] is None:
+        return {"message": "Invalid text! Please try again!."}, 400
+    return response
